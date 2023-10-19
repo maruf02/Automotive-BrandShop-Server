aff6ee9dd4b2f44bedfcff0624658913044a3c70
maruf02 <rashiduzzaman99@gmail.com>
2023-10-19 17:09:55 +0600

    Note: Mongodb Connect with environment variable

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -30,19 +30,87 @@ async function run() {
         await client.connect();
 
 
-        const CarCollection = client.db('BrandShopDB').collection('Brands');
+        const AllCarBrandCollection = client.db('BrandShopDB').collection('Brands');
+        const AllCarCollection = client.db('BrandShopDB').collection('allCars');
+        // for all car brands
+        app.get('/Brands', async (req, res) => {
+            const cursor = AllCarBrandCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
+        app.get('/Brands/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AllCarBrandCollection.findOne(query);
+            res.send(result);
+        })
 
         app.post('/Brands', async (req, res) => {
             const newBrand = req.body;
             console.log(newBrand);
-            const result = await CarCollection.insertOne(newBrand);
+            const result = await AllCarBrandCollection.insertOne(newBrand);
+            res.send(result);
+        })
+
+        app.put('/Brands/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) }
+            const option = { upsert: true }
+            const updateBrand = req.body;
+            const Brand = {
+                $set: {
+                    BrandName: updateBrand.BrandName,
+                    BrandImage: updateBrand.BrandImage
+                }
+            }
+
+            const result = await AllCarBrandCollection.updateOne(filter, Brand, option);
+            res.send(result);
+        })
+
+        app.delete('/Brands/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await AllCarBrandCollection.deleteOne(query);
             res.send(result);
         })
 
 
+        // for all car brands
+        // ************************************
+        // for all cars
 
 
+        app.get('/Cars', async (req, res) => {
+            const cursor = AllCarCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/Cars/:brand', async (req, res) => {
+            const brand = req.params.brand;
+            const cursor = AllCarCollection.find({ brandName: brand });
+            const result = await cursor.toArray();
+            console.log(result);
+            res.send(result);
+        });
+
+        app.get('/Cars/:brand/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await AllCarCollection.findOne(query);
+            console.log(result);
+            res.send(result);
+        })
+
+        app.post('/Cars', async (req, res) => {
+            const newCar = req.body;
+            console.log(newCar);
+            const result = await AllCarCollection.insertOne(newCar);
+            res.send(result);
+        })
+        // for all cars
 
 
 
